6a7d5921eb4a4a93177adaf55426e5ebcb6d6c80
romain <rom1@canel.ch>
2020-02-07 22:00:43 +0100

    Initialisation.

--- bosp.py
+++ bosp.py
@@ -1,44 +1,58 @@
 #!/usr/bin/env python3
-import os
 import RPi.GPIO as GPIO
 import signal
-import subprocess
 import sys
 import time
+from omxplayer.player import OMXPlayer
 
-#video='/home/bolo/Vidéos/ys-s03e12.avi'
-mp_fifo_f = "/tmp/mpc"
+video='/home/bolo/Vidéos/robin_masur_4_3.mp4'
+video='/home/bolo/Vidéos/Adola_Fofana_test_barre_noire.mp4'
+intro_delay=3 # Délais pour bloquer le bouton pour qu'il évite de se répéter.
+intro_lock=False
 
-#os.system('su bolo -c "DISPLAY=:0 mplayer {file} -input file={fifo}"'.format(file = video, fifo = mp_fifo_f))
-#exec('mplayer', video, '-input', 'file={}'.format(mp_fifo_f))
+player = OMXPlayer(video,
+        args=['--no-osd', '-o', 'local', '--loop', '--align', 'center'],
+        dbus_name='org.mpris.MediaPlayer2.omxplayer',
+        pause=True)
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 bouncetime=300
 gpio_led = 23
 GPIO.setup(gpio_led, GPIO.OUT)
+GPIO.output(gpio_led, GPIO.HIGH)
 gpio_but = 24
 GPIO.setup(gpio_but, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def send_cmd(cmd):
-    print('Command: {}'.format(cmd))
-    with open(mp_fifo_f, 'w') as f:
-        f.write('{}\n'.format(cmd))
 
-def pause(ch):
-    #send_cmd('stop')
-    #send_cmd('pause')
-    #send_cmd('loadfile {} 0'.format(video))
-    send_cmd('seek 0 2')
-GPIO.add_event_detect(gpio_but, GPIO.RISING, callback=pause, bouncetime=bouncetime)
+def restart(ch):
+    global intro_lock
+    print("Push")
+    if int(player.position()) > intro_delay or intro_lock == False:
+        print("Restart")
+        if player.can_pause():
+            player.pause()
+            player.set_position(0.0)
+        if player.can_play():
+            player.play()
+        GPIO.output(gpio_led, GPIO.LOW)
+        intro_lock = True
+GPIO.add_event_detect(gpio_but, GPIO.RISING, callback=restart, bouncetime=bouncetime)
 
 def signal_handler(sig, frame):
-    send_cmd('quit 1')
+    player.quit()
     print('\nYou pressed Ctrl+C!\n')
     sys.exit(0)
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal_handler)
-    signal.pause()
-    #while True:
-     #   time.sleep(0.01)
+    while True:
+        if int(player.position()) == int(player.duration()):
+            player.pause()
+            player.set_position(0.0)
+            player.play()
+            time.sleep(0.5)
+            player.pause()
+            GPIO.output(gpio_led, GPIO.HIGH)
+            intro_lock = False
+        time.sleep(0.1)
